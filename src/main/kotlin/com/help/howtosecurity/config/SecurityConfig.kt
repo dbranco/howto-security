485e31d36d4953dc906970a89e3f889265826d28
Geoffrey Rekier <geoffrey@osnode.com>
2021-08-04 12:58:13 +0200

    Fix multiple oauth

--- src/main/kotlin/com/help/howtosecurity/config/SecurityConfig.kt
+++ src/main/kotlin/com/help/howtosecurity/config/SecurityConfig.kt
@@ -1,8 +1,10 @@
 package com.help.howtosecurity.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.authorization.AuthorizationDecision
@@ -11,80 +13,68 @@ import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
-
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers
+import org.springframework.web.reactive.config.EnableWebFlux
 import reactor.core.publisher.Mono
 
 
 @Configuration
 @EnableWebFluxSecurity
+@EnableWebFlux
 class SecurityConfig {
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    var issuerUri: String? = null
 
     @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+    @Order(1)
+    fun permitAllWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
-            .httpBasic().authenticationManager { basicAuthenticationDummyAuthentication(it) }.and()
-            .oauth2ResourceServer().jwt().and().and()
+            .httpBasic().disable()
+            .securityMatcher(pathMatchers("/permitall/**"))
             .authorizeExchange()
             .pathMatchers("/permitall/**").permitAll()
-            .pathMatchers("/securebasic/**").access{
-                    authMono, _ -> authMono.map { authentication ->
-                canAccess(authentication, "basic")
-            }
-            }
-            .pathMatchers("/secureoauth/**").access{
-                authMono, _ -> authMono.map { authentication ->
-                    canAccess(authentication, "dummy")
-                }
-            }
             .and().build()
     }
 
-//    @Bean
-//    @Order(1)
-//    fun permitAllWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-//        return http
-//            .authorizeExchange()
-//            .pathMatchers("/permitall/**").permitAll()
-//            .and().build()
-//    }
-
-//    @Bean
-//    @Order(2)
-//    fun basicAuthWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-//        return http
-//            .httpBasic().authenticationManager { basicAuthenticationDummyAuthentication(it) }.and()
-//            .authorizeExchange()
-//            .pathMatchers("/securebasic/**").access {
-//                authMono, _ -> authMono.map { authentication ->
-//                    canAccess(authentication, "basic")
-//                }
-//            }
-//            .and().build()
-//    }
-
-//    @Bean
-//    @Order(3)
-//    fun oAuthWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-//        return http
-//            .oauth2ResourceServer().jwt().and().and()
-//            .authorizeExchange()
-//            .pathMatchers("/secureoauth/**").authenticated()
-//            .and().build()
-//    }
-
-    private fun canAccess(authentication: Authentication, nameToCompare: String): AuthorizationDecision {
-        when(authentication) {
-            is OAuth2AuthenticationToken -> {
-                if ((authentication.principal as DefaultOidcUser).userInfo.givenName == nameToCompare) {
-                    return AuthorizationDecision(true)
+    @Bean
+    @Order(2)
+    fun basicAuthWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        return http
+            .securityMatcher(pathMatchers("/securebasic/**"))
+            .httpBasic().authenticationManager { basicAuthenticationDummyAuthentication(it) }.and()
+            .authorizeExchange { spec ->
+                run {
+                    spec.pathMatchers("/securebasic/**").authenticated()
                 }
             }
-        }
-        if (authentication.name == nameToCompare) {
-            return AuthorizationDecision(true)
-        }
-        return AuthorizationDecision(false)
+            .exceptionHandling()
+            .authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and()
+            .build()
+    }
+
+    @Bean
+    @Order(3)
+    fun oAuthWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .httpBasic().disable()
+            .securityMatcher(pathMatchers("/secureoauth/**"))
+            .authorizeExchange { spec ->
+                run {
+                    spec.pathMatchers("/secureoauth/**").authenticated()
+                }
+            }
+            .oauth2ResourceServer(
+                ServerHttpSecurity.OAuth2ResourceServerSpec::jwt
+            )
+            .exceptionHandling()
+            .authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and().build()
     }
 
     private fun basicAuthenticationDummyAuthentication(it: Authentication?) = when (it) {
@@ -95,6 +85,11 @@ class SecurityConfig {
             }
         }
         else -> Mono.empty()
+    }
+
+    @Bean
+    fun reactiveJwtDecoder(): ReactiveJwtDecoder? {
+        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri)
     }
 
 }
